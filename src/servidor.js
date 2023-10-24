ebba44d7b6b115f30e5068014b47cd4e391e92a7
Ygor Fortes <ygor1888@hotmail.com>
2023-10-24 18:18:53 -0300

    Criado servidor e conectado com o socket.io

---
+++ src/servidor.js
@@ -0,0 +1,33 @@
+import  express  from "express";
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const port = 3000;
+
+//Buscando o caminho atual do projeto
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+
+//Criando o diretorio para a paginas html
+const diretorioPublico = path.join(caminhoAtual, "../..","./public");
+
+//Colocando as paginas html em servidor de forma statica
+app.use(express.static(diretorioPublico));
+
+const servidorHttp =  http.createServer(app);
+
+
+servidorHttp.listen(port, ()=>{
+  console.log(`Servidor funcionando na porta http://localhost:${port}/`);
+});
+
+
+const io  = new Server(servidorHttp);
+
+io.on('connection', ()=> {
+  console.log('O cliente se conectou');
+});
+
